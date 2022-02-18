fd003016746b2fc0f945d78a47629f41837a2c07
Joel-dev-IMP <Joel-dev-IMP@users.noreply.github.com>
2022-02-18 10:41:23 +0000

    Update the addon.

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ import bpy
 bl_info = {
     "name": "Demo Addon 4",
     "author": "Joel Benkwitz (BD)",
-    "version": (1, 0, 1),
+    "version": (1, 0, 2),
     "blender": (2, 83, 0),
     "warning": "",
     "tracker_url": "https://github.com/Joel-dev-IMP/jufo-oeff-5/issues/new",
